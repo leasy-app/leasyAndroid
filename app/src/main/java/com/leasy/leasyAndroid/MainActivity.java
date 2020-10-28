a6988d12d25f6235773f25e3c0446fc8123552e4
nimahsn <nimahsn@hotmail.com>
2020-10-29 01:30:31 +0330

    Main Activity and Main Fragment added main activity hosts all other pages and fragments. Main Fragment will host Home, profile, post, categories.

---
+++ app/src/main/java/com/leasy/leasyAndroid/MainActivity.java
@@ -0,0 +1,21 @@
+package com.leasy.leasyAndroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.leasy.leasyAndroid.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container_main_activity, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
