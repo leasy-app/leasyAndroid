2af96c8b23bda337d473e5617960c599800f2491
nimahsn <nimahsn@hotmail.com>
2020-10-29 17:45:53 +0330

    Botton Navigation added to Main Fragment

--- app/src/main/java/com/leasy/leasyAndroid/ui/main/MainFragment.java
+++ app/src/main/java/com/leasy/leasyAndroid/ui/main/MainFragment.java
@@ -10,14 +10,17 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelStoreOwner;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.leasy.leasyAndroid.R;
 
-public class MainFragment extends Fragment implements ViewModelStoreOwner {
+public class MainFragment extends Fragment implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private MainViewModel mainViewModel;
+    private BottomNavigationView bottomNav;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -25,9 +28,17 @@ public class MainFragment extends Fragment implements ViewModelStoreOwner {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.main_fragment, container, false);
+        initialize(v);
+        bottomNav.setOnNavigationItemSelectedListener(this);
+        return v;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // TODO: 10/29/20
+        return false;
     }
 
     @Override
@@ -35,6 +46,10 @@ public class MainFragment extends Fragment implements ViewModelStoreOwner {
         super.onActivityCreated(savedInstanceState);
         mainViewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    private void initialize(View v) {
+        bottomNav = v.findViewById(R.id.bottom_nav_main);
     }
 
 }
