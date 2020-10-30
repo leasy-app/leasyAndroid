87b453ea607552db09aefe514f5294824b6304d8
nimahsn <nimahsn@hotmail.com>
2020-10-30 15:13:54 +0330

    main fragment methods added : bottom nav listener fragment changer

--- app/src/main/java/com/leasy/leasyAndroid/ui/main/MainFragment.java
+++ app/src/main/java/com/leasy/leasyAndroid/ui/main/MainFragment.java
@@ -1,5 +1,7 @@
 package com.leasy.leasyAndroid.ui.main;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -26,6 +28,8 @@ public class MainFragment extends Fragment implements BottomNavigationView.OnNav
         return new MainFragment();
     }
 
+    enum EnumDir {rightToLeft, leftToRight};
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -37,8 +41,53 @@ public class MainFragment extends Fragment implements BottomNavigationView.OnNav
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // TODO: 10/29/20
-        return false;
+        if (item.getItemId() == bottomNav.getSelectedItemId()) {
+            return true;
+        }
+
+        if (item.getItemId() == R.id.home_bottom_nav_main){
+            loadHomePage();
+        } else if (item.getItemId() == R.id.write_bottom_nav_main){
+            loadWritePage();
+        } else if (item.getItemId() == R.id.categories_bottom_nav_main){
+            loadCategoriesPage();
+        } else if (item.getItemId() == R.id.profile_bottom_nav_main){
+            loadProfilePage();
+        }
+
+        return true;
+    }
+
+    private void changeMainFragment(Fragment newFragment, @Nullable EnumDir transitionDir, boolean addToBackStack){
+        FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
+        if (transitionDir == EnumDir.rightToLeft){
+            fragmentTransaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right,
+                    R.anim.slide_in_right, R.anim.slide_out_left);
+        } else if (transitionDir == EnumDir.leftToRight){
+            fragmentTransaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left,
+                    android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+        }
+        if (addToBackStack){
+            fragmentTransaction.addToBackStack(null);
+        }
+        fragmentTransaction.replace(R.id.container_main_fragment, newFragment);
+        fragmentTransaction.commitNow();
+    }
+
+    private void loadHomePage(){
+        // TODO: 10/30/20
+    }
+
+    private void loadCategoriesPage(){
+        // TODO: 10/30/20
+    }
+
+    private void loadProfilePage(){
+        // TODO: 10/30/20
+    }
+
+    private void loadWritePage(){
+        // TODO: 10/30/20
     }
 
     @Override
