8278a134f6c20477115ff4876489dc1821be76de
nimahsn <nimahsn@hotmail.com>
2020-10-31 01:59:40 +0330

    glide library added

--- app/src/main/java/com/leasy/leasyAndroid/ui/main/HomeFragment.java
+++ app/src/main/java/com/leasy/leasyAndroid/ui/main/HomeFragment.java
@@ -4,20 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.bumptech.glide.Glide;
 import com.leasy.leasyAndroid.R;
 
 public class HomeFragment extends Fragment {
+
+    private ImageView imgUser;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        initialize(v);
+
+        Glide.with(this).load(R.drawable.profile).centerInside().circleCrop().into(imgUser);
+
+        return v;
+    }
+
+    private void initialize(View v) {
+        imgUser = v.findViewById(R.id.img_user_image_home);
     }
 }
