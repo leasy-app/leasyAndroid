446db913e891c8ac8b09fe67b8a7befc641d5a93
nimahsn <nimahsn@hotmail.com>
2020-11-25 03:59:45 +0330

    HomeFragment ui fixes: collapsing toolbar replaced with regular toolbar, SwipeRefresh layout added

--- app/src/main/java/com/leasy/leasyAndroid/ui/main/HomeFragment.java
+++ app/src/main/java/com/leasy/leasyAndroid/ui/main/HomeFragment.java
@@ -1,18 +1,17 @@
 package com.leasy.leasyAndroid.ui.main;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.bumptech.glide.Glide;
 import com.leasy.leasyAndroid.MainActivity;
@@ -32,6 +31,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener, UiCa
     private ImageView imgUser;
     private RecyclerView recyclerPosts;
     private RecyclerAdapterPostsVertical recyclerAdapter;
+    private SwipeRefreshLayout refreshLayout;
     private List<PostsListItem> postsListItemList;
 
     @Nullable
@@ -41,16 +41,10 @@ public class HomeFragment extends Fragment implements View.OnClickListener, UiCa
                              @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
         initialize(v);
+        refreshLayout.setRefreshing(true);
 
-        Glide.with(this).load(R.drawable.profile).centerInside().into(imgUser);
+//        Glide.with(this).load(R.drawable.profile).centerInside().into(imgUser);
 
-
-        // FIXME: 10/31/20 Get the real list
-
-//        LinkedList<ListItems> list = new LinkedList<>();
-//        ListItems items = new ListItems();
-//        items.setSectionTitle("Latest Posts");
-//        list.add(items);
         if (postsListItemList == null) {
             ApiUtils.requestGetAllPosts(this, null);
         } else {
@@ -58,6 +52,13 @@ public class HomeFragment extends Fragment implements View.OnClickListener, UiCa
             recyclerPosts.setHasFixedSize(true);
             recyclerPosts.setAdapter(recyclerAdapter);
         }
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                ApiUtils.requestGetAllPosts(HomeFragment.this, null);
+            }
+        });
 
         return v;
     }
@@ -69,6 +70,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener, UiCa
     }
 
     private void initialize(View v) {
+        refreshLayout = v.findViewById(R.id.swipe_refresh_home_posts);
         imgUser = v.findViewById(R.id.img_user_image_home);
         recyclerPosts = v.findViewById(R.id.recycler_home_page);
     }
@@ -77,17 +79,18 @@ public class HomeFragment extends Fragment implements View.OnClickListener, UiCa
     public void onRequestSuccessful(Response response) {
         postsListItemList = new LinkedList<>();
         postsListItemList.add(new PostsListItem("Latest Posts"));
-        for (PostsListItem.PostItem post :((List<PostsListItem.PostItem>) response.body())){
-            Log.i("logloglog", post.toString());
+        for (PostsListItem.PostItem post : ((List<PostsListItem.PostItem>) response.body())) {
             postsListItemList.add(new PostsListItem(post));
         }
         recyclerAdapter = new RecyclerAdapterPostsVertical(this::onClick, postsListItemList, getContext());
         recyclerPosts.setHasFixedSize(true);
         recyclerPosts.setAdapter(recyclerAdapter);
+        refreshLayout.setRefreshing(false);
     }
 
     @Override
     public void onRequestError(Response response) {
+        refreshLayout.setRefreshing(false);
 
         Toast.makeText(getContext(), "req error", Toast.LENGTH_LONG).show();
     }
@@ -95,25 +98,31 @@ public class HomeFragment extends Fragment implements View.OnClickListener, UiCa
     @Override
     public void onRequestSendFailure(Throwable t) {
         Toast.makeText(getContext(), "send error", Toast.LENGTH_LONG).show();
+        refreshLayout.setRefreshing(false);
+
     }
 
     @Override
     public void onRefreshTokenExpired(Response response) {
+        refreshLayout.setRefreshing(false);
 
     }
 
     @Override
     public void onObtainAccessTokenError(Response response) {
+        refreshLayout.setRefreshing(false);
 
     }
 
     @Override
     public void onObtainAccessTokenFailure(Throwable t) {
+        refreshLayout.setRefreshing(false);
 
     }
 
     @Override
     public void onInternalErrorFailure() {
+        refreshLayout.setRefreshing(false);
 
     }
 }
